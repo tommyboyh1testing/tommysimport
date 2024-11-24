347ac66a84db556f47653a0a7eda3c249b569d09
thomas boyd <tommyboyhacking@wearehackerone.com>
2024-11-24 12:00:03 -0500

    Files uploaded
    Extended Option

---
+++ installweb3deps.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Script to install commonly found web3 dependencies / libraries
+
+# Open in current working Hardhat folder
+
+npm install @openzeppelin/contracts
+npm install @chainlink/contracts
+npm install solmate
